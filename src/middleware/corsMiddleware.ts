ea20a4d6eb10a4c039b3d077a3c21b6481fef712
udhai-20 <udhayaPococ>
2025-03-03 17:27:17 +0530

    fix:cors url added for local

--- src/middleware/corsMiddleware.ts
+++ src/middleware/corsMiddleware.ts
@@ -4,27 +4,27 @@ import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    const origin = req.headers.origin as string || "https://gen-ai-blogs.netlify.app/";
-    const domain = new URL(origin).hostname; // Extracting the domain from the origin
-    // console.log('domain:', domain);
-    // console.log('origin:', origin);
+    use(req: Request, res: Response, next: NextFunction) {
+        const origin = req.headers.origin as string || "https://gen-ai-blogs.netlify.app/";
+        const domain = new URL(origin).hostname; // Extracting the domain from the origin
+        // console.log('domain:', domain);
+        // console.log('origin:', origin);
 
-if (origin.startsWith('http://localhost:3000') || origin.startsWith('http://localhost:3001') || origin.startsWith('https://gen-ai-blogs.netlify.app/')) {
-      res.header('Access-Control-Allow-Origin', origin);
+        if (origin.startsWith('http://localhost:3000') || origin.startsWith('http://localhost:3001') || origin.startsWith('http://localhost:5173') || origin.startsWith('https://gen-ai-blogs.netlify.app/')) {
+            res.header('Access-Control-Allow-Origin', origin);
+        }
+
+        res.header(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PUT, DELETE, OPTIONS, PATCH',
+        );
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        if (req.method === 'OPTIONS') {
+            // Handle preflight requests
+            res.status(200).end();
+        } else {
+            next();
+        }
     }
-
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS, PATCH',
-    );
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    if (req.method === 'OPTIONS') {
-      // Handle preflight requests
-      res.status(200).end();
-    } else {
-      next();
-    }
-  }
 }
